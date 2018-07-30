3b02f231dea748783768b14cd319b9a99655c4d6
Julius Fan <jfan@spigot.com>
2018-07-30 15:10:31 -0400

    first commit

---
+++ PushDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PushDashboard.Startup))]
+namespace PushDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
